2160867a52f20b5bd38ad11b1fa6adc5d7688342
eryner-z <stfn.bgr@gmail.com>
2022-03-21 18:04:02 +0100

    Add two components to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
 
 class App extends React.Component {
   render() {
@@ -6,7 +8,12 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <h1>Hello World!</h1>
+            <Header></Header>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home></Home>
           </div>
         </div>
       </div>
